354a6d88754ebc24b946f859554f0273f9cea8e3
arham51017027 <48231420+arham51017027@users.noreply.github.com>
2019-05-13 22:27:58 +0800

    Add files via upload

---
+++ part3.cpp
@@ -0,0 +1,19 @@
+#include <iostream.>
+#include <conio.h>
+
+main()
+{
+int a,b,c=1;
+for(a=5; a>=1; a--)
+{
+for(b=1; b<=a; b++)
+{
+cout<<c<<" " ;
+c++;
+
+}
+cout<<"\n";
+cout<<"\n";
+}
+getch();
+}
---
+++ part5.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+#include <conio.h>
+main()
+{
+int a;
+int b;
+for(a=1; a<=4; a++)
+{
+for(b=1; b<=4; b++)
+{
+if(a==b){
+cout<<a<<" ";
+}
+}
+Cout<<"\n";
+cout<<"\n";
+}
+getch();}
